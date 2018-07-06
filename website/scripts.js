2bdbb1948420fa6d38c33f864dbfbda81f5f5f25
Mirza Leka <mirza_376@hotmail.com>
2018-07-06 15:31:27 +0200

    Work so far

--- website/scripts.js
+++ website/scripts.js
@@ -1,13 +1,46 @@
 $(document).ready(() => {
 
-
     $.ajax({
     type: 'GET',
     url: '/all',
     success: function(data) {
 
-        console.log(data);
+        let keys = Object.keys(data);
+        let values = Object.values(data);
 
+    let response = '';
+    for (let i = 0; i < keys.length; i++) {
+    response+= `<ul class="inner"> <li>${keys[i]}</li> <li>${values[i]}</li> </ul>`;
+        }
+
+    $(".outer").append(response);
+
+    },
+    error: function() {
+        console.log("Bad request!");
+    }
+    
+    });
+    
+    });
+
+    function submitWord() {
+        var word = $("input[type='text']").val();
+        var score = $("input[type='number']").val();
+
+/*
+  $.ajax({
+    type: 'POST',
+    url: `/add/TM/87`,
+     success: function() {
+      console.log("Hello world!");
+    }
+        });
+
+       */
+         }
+
+    /*
         // define size variable
         Object.size = function(obj) {
             var size = 0, key;
@@ -19,27 +52,4 @@ $(document).ready(() => {
         
         // Get the size of an object
         var size = Object.size(data);
-        console.log("Size is " + size)
-
-    // keys.length = size = num of objects
-
-    // GET keys (properties) of all objects
-        var keys = Object.keys(data);
-        console.log("keys: " + keys);
-
-    // GET values of all keys
-        var values = Object.values(data);
-        console.log("values: " + values);
-
-var response = '';
-for (var i = 0; i < keys.length; i++) {
-response+= `<ul class="inner"> <li>${keys[i]}</li> <li>${values[i]}</li> </ul>`;
-}
-
-$(".outer").append(response);
-
-    }
-    
-    });
-    
-    });
+        */
