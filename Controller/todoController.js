0cfdafd816053ec5721883be98aa8dcf0703935b
Mirza Leka <mirza_376@hotmail.com>
2018-07-25 21:17:10 +0200

    Added dummy database. Setup Post and Delete

--- Controller/todoController.js
+++ Controller/todoController.js
@@ -1,15 +1,28 @@
+const bodyParser = require('body-parser');
+let urlencodedParser = bodyParser.urlencoded({extended: false});
+
+let data = [
+    {item: "get milk"},
+    {item: "walk a dog"},
+    {item: "kick some ass"}
+];
+
 module.exports = (app) => {
 
 app.get('/todo', (req, res) => {
-res.render('todo');
+res.render('todo', {todos: data});
 });
 
-app.post('/todo', (req, res) => {
-
+app.post('/todo', urlencodedParser, (req, res) => {
+data.push(req.body);
+res.json(data);
 });
 
-app.delete('/todo', (req, res) => {
-
+app.delete('/todo/:item', (req, res) => {
+    data = data.filter(function(todo) {
+        return todo.item.replace(/ /g, '-' ) != req.params.item;
+    });
+    res.json(data);
 });
 
 
