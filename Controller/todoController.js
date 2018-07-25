b88a90c32f658fe99f411b5adb3ddcf100673707
Mirza Leka <mirza_376@hotmail.com>
2018-07-25 22:35:20 +0200

    Connected to DB via mLab. Saved item in DB

--- Controller/todoController.js
+++ Controller/todoController.js
@@ -1,5 +1,27 @@
 const bodyParser = require('body-parser');
 let urlencodedParser = bodyParser.urlencoded({extended: false});
+const mongoose = require('mongoose');
+
+// Connect to the databse
+
+mongoose.connect('mongodb://test:test123@ds155461.mlab.com:55461/todo');
+
+// Create a schema - this is like a blueprint
+
+var todoSchema = new mongoose.Schema({
+item: String
+});
+
+// Creating a Todo Model based of previously created schema
+
+var ToDo = mongoose.model('Todo', todoSchema);
+var itemOne = ToDo({item: "buy flowers"}).save((err) => {
+if (err) {
+    return console.log(err);
+}
+console.log("Item saved!");
+
+});
 
 let data = [
     {item: "get milk"},
